47a67b0ae4576ea47d4badbb2678ce7817f87363
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2024-07-28 16:53:45 -0300

    feat: JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/guilhermecustodio/demo/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.guilhermecustodio.demo.repositories;
+
+import com.guilhermecustodio.demo.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
